f3758c388eee05bb23c0365d2c0ff4640dafcf08
Fillipe <fillipe484@gmail.com>
2021-08-05 15:08:58 -0300

    feat: set default theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import defaultTheme from './styles/themes/default';
 import Routes from './routes/Routes';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes />
+      <ThemeProvider theme={defaultTheme}>
+        <Routes />
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
