8963d4ea19921e95af255068a32eb0544a9d0851
Fillipe <fillipe484@gmail.com>
2021-08-13 12:29:04 -0300

    feat: Adjust Cards Responsivness

--- src/utils/SliderSettings.js
+++ src/utils/SliderSettings.js
@@ -11,33 +11,21 @@ const settings = {
   adaptiveHeight: true,
   prevArrow: <IoIosArrowBack color="white" />,
   nextArrow: <IoIosArrowForward color="white" />,
-  mobileFirst: true,
-  // responsive: [
-  //   {
-  //     breakpoint: 1024,
-  //     settings: {
-  //       slidesToShow: 3,
-  //       slidesToScroll: 3,
-  //       infinite: true,
-  //       dots: true,
-  //     },
-  //   },
-  //   {
-  //     breakpoint: 600,
-  //     settings: {
-  //       slidesToShow: 2,
-  //       slidesToScroll: 2,
-  //       initialSlide: 2,
-  //     },
-  //   },
-  //   {
-  //     breakpoint: 480,
-  //     settings: {
-  //       slidesToShow: 1,
-  //       slidesToScroll: 1,
-  //     },
-  //   },
-  // ],
+  responsive: [
+    {
+      breakpoint: 480,
+      settings: {
+        dots: false,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        initialSlide: 0,
+        variableWidth: true,
+        adaptiveHeight: true,
+      },
+    },
+  ],
 };
 
 export default settings;
